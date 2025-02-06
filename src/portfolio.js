be1cefe6fdf878429e21e327d0a75539de2d2bba
Don Michael <155391757+DonOmbisi@users.noreply.github.com>
2025-02-06 22:37:26 +0300

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ const greeting = {
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
-    "https://drive.google.com/file/d/189zQYUR5Q7wqNyX1wiiKpqYBnPzUd2YK/view",
+    "https://drive.google.com/file/d/1UtkR7SlgFt3EJs944Me_bwLQsAj2vDkH/view?usp=sharing",
   portfolio_repository: "https://github.com/DonOmbisi/portfolio-m",
   githubProfile: "https://github.com/DonOmbisi",
 };
